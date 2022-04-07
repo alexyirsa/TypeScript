36924792e1361870d815ed2401abbdffc2d0e9a0
AlexanderYirsa <v.yirsa@uniandes.edu.co>
2022-04-07 06:35:00 -0500

    Tercer commit, incluida configurción

---
+++ scripts/aprendiz.js
@@ -0,0 +1,17 @@
+export var NivelEducativo;
+(function (NivelEducativo) {
+    NivelEducativo["BACHILLERATO"] = "Bachillerato";
+    NivelEducativo["UNIVERSITARIO"] = "Universitario";
+    NivelEducativo["POSTGRADO"] = "Postgrado";
+})(NivelEducativo || (NivelEducativo = {}));
+var Aprendiz = /** @class */ (function () {
+    function Aprendiz(nombres, apellidos, avatar, edad, nivelEducativo) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.avatar = avatar;
+        this.edad = edad;
+        this.nivelEducativo = nivelEducativo;
+    }
+    return Aprendiz;
+}());
+export { Aprendiz };
---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+import { Aprendiz, NivelEducativo } from './aprendiz.js';
+export var ap = new Aprendiz("Alexander", "Yirsa", "avatar.png", 44, NivelEducativo.POSTGRADO);
+console.log(ap);
