4b9effe8f17e42ca7f0d15cca85e19d17da8ede9
AlexanderYirsa <v.yirsa@uniandes.edu.co>
2022-04-07 07:10:40 -0500

    Cuarto Commit de mi app

--- main.ts
+++ main.ts
@@ -2,4 +2,17 @@ import {Aprendiz, NivelEducativo} from './aprendiz.js'
 
 export const ap = new Aprendiz("Alexander", "Yirsa", "avatar.png", 44, NivelEducativo.POSTGRADO);
 
-console.log(ap);
+let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+
+mostrarDatosAprendiz(ap);
+
+function mostrarDatosAprendiz(aprendiz: Aprendiz): void{
+    let tbodyAprendiz = document.createElement("tbody");
+    tbodyAprendiz.innerHTML = `<tr><td colspan=2><img src="./${aprendiz.avatar}" height="100"></td></tr>
+    <tr><td>Nombres:</td><td>${aprendiz.nombres}</td></tr>
+    <tr><td>Apellidos:</td><td>${aprendiz.apellidos}</td></tr>
+    <tr><td>Nivel Educativo:</td><td>${aprendiz.nivelEducativo}</td></tr>
+    <tr><td>Edad:</td><td>${aprendiz.edad}</td></tr>`
+    aprendizTable.appendChild(tbodyAprendiz);
+
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 import { Aprendiz, NivelEducativo } from './aprendiz.js';
 export var ap = new Aprendiz("Alexander", "Yirsa", "avatar.png", 44, NivelEducativo.POSTGRADO);
-console.log(ap);
+var aprendizTable = document.getElementById("aprendiz");
+mostrarDatosAprendiz(ap);
+function mostrarDatosAprendiz(aprendiz) {
+    var tbodyAprendiz = document.createElement("tbody");
+    tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./".concat(aprendiz.avatar, "\" height=\"100\"></td></tr>\n    <tr><td>Nombres:</td><td>").concat(aprendiz.nombres, "</td></tr>\n    <tr><td>Apellidos:</td><td>").concat(aprendiz.apellidos, "</td></tr>\n    <tr><td>Nivel Educativo:</td><td>").concat(aprendiz.nivelEducativo, "</td></tr>\n    <tr><td>Edad:</td><td>").concat(aprendiz.edad, "</td></tr>");
+    aprendizTable.appendChild(tbodyAprendiz);
+}
