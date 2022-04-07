0a892f30aa83a7e0decc1298d333fbbc02d9f092
AlexanderYirsa <v.yirsa@uniandes.edu.co>
2022-04-07 06:13:57 -0500

    Segundo commit de mi app

--- aprendiz.ts
+++ aprendiz.ts
@@ -6,7 +6,7 @@ export enum NivelEducativo{
 }
 export class Aprendiz{
 
-    constructor(public nombres: string, public apellidos: string, public avatar: string, public edad: number){
+    constructor(public nombres: string, public apellidos: string, public avatar: string, public edad: number, public nivelEducativo: NivelEducativo){
 
     }
 }
---
+++ main.ts
@@ -0,0 +1,5 @@
+import {Aprendiz, NivelEducativo} from './aprendiz.js'
+
+export const ap = new Aprendiz("Alexander", "Yirsa", "avatar.png", 44, NivelEducativo.POSTGRADO);
+
+console.log(ap);
